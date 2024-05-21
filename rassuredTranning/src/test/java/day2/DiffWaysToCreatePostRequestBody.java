8ab2d45cb7e3a52a3059f3bb65f1c65f85cd3cdc
gabito <gabito.onj@gmail.com>
2024-05-21 00:29:47 -0300

    Session 2

--- rassuredTranning/src/test/java/day2/DiffWaysToCreatePostRequestBody.java
+++ rassuredTranning/src/test/java/day2/DiffWaysToCreatePostRequestBody.java
@@ -1,22 +1,30 @@
 package day2;
 
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.annotations.Test;
 
+//import io.restassured.internal.support.FileReader;
 import io.restassured.response.ValidatableResponse;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Reader;
 import java.util.HashMap;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 
 
 public class DiffWaysToCreatePostRequestBody {
 
-	// post request body using Hashmap
+	// #1 post request body using Hashmap
 	
 	@Test(priority=1)
 	void testPostusingHashMap() {
@@ -25,10 +33,11 @@ public class DiffWaysToCreatePostRequestBody {
         data.put("name", "Gabito");
         data.put("location", "BUE");
         data.put("phone", "1146963030");
-        // data.put("job", "QA");
-        //String courseArr[]= {"C","C++"};
-        //data.put("courses", courseArr);
-        
+        data.put("job", "QA");
+        //String courseArr[]= {"C","JAVA"};
+        //data.put("courses", courseArr[0]);
+        //data.put("courses", courseArr[1]);
+
         given()
       	      .contentType("application/json")
       	      .body(data)
@@ -41,8 +50,8 @@ public class DiffWaysToCreatePostRequestBody {
                .body("location", equalTo("BUE"))
                .body("phone", equalTo("1146963030"))
                //.body("courses[0]", equalTo("C"))
-               //.body("courses[1]", equalTo("C++"))
-               //.header("Content-type","application/json; charset=utf-8")
+               //.body("courses[1]", equalTo("JAVA"))
+               //.header("Content-Type","application/json; charset=utf-8")
                .log().all();
 
 	}
@@ -57,4 +66,102 @@ public class DiffWaysToCreatePostRequestBody {
     	      .delete("http://localhost:3000/students/6")
   	      .then()
               .statusCode(404);
-	}}
+	}
+	
+	// #2 post request body using org.json lib
+	
+		@Test(priority=3)
+		void testPostusingJsonLib() {
+	    	
+	    JSONObject data=new JSONObject();
+	    
+        data.put("name", "Gab");
+        data.put("location", "France");
+        data.put("phone", "1111111111");
+        data.put("job", "QAE");
+        
+        System.out.println(data);
+        
+	        given()
+	      	      .contentType("application/json")
+	      	      .body(data.toString())
+	      	 
+	      	  .when()
+	      	      .post("http://localhost:3000/students")
+	      	  .then()
+	               .statusCode(201)
+	               .body("name", equalTo("Gab"))
+	               .body("location", equalTo("France"))
+	               .body("phone", equalTo("1111111111"))
+	               .log().all();
+
+		}
+		
+		@Test(priority=4)
+	    public void deleteUser2() {
+	    	
+	    	
+	    	given()
+	    	 
+	    	  .when()
+	    	    .delete("http://localhost:3000/students/7")
+	  	      .then()
+	              .statusCode(404);
+		}
+		
+		// #3 POJO (Plain Old Java Object)
+		
+		@Test(priority=5)
+		void testPostusingPOJO() {
+	    	
+	    
+		PojoPostRequest data= new PojoPostRequest();
+        
+		data.setName("GabOld");
+        data.setLocation("FranceOld");
+        data.setPhone("1111111111Old");
+        data.setJob("QAEold");
+        
+        
+	        given()
+	      	      .contentType("application/json")
+	      	      .body(data)
+	      	 
+	      	  .when()
+	      	      .post("http://localhost:3000/students")
+	      	  .then()
+	               .statusCode(201)
+	               .body("name", equalTo("GabOld"))
+	               .body("location", equalTo("FranceOld"))
+	               .body("phone", equalTo("1111111111Old"))
+	               .log().all();
+
+		}
+		
+		// #4 Json external file 
+		
+				@Test(priority=6)
+				void testPostusingExternalJsonFile() throws FileNotFoundException {
+			    	
+			    File f = new File(".//body.json");
+			    FileReader fr = new FileReader(f);
+			    
+			    JSONTokener jt = new JSONTokener(fr);
+			    
+			    JSONObject data = new JSONObject(jt);
+		        
+			        given()
+			      	      .contentType("application/json")
+			      	      .body(data.toString())
+			      	 
+			      	  .when()
+			      	      .post("http://localhost:3000/students")
+			      	  .then()
+			               .statusCode(201)
+			               .body("name", equalTo("GabjsonFile"))
+			               .body("location", equalTo("FrancejsonFile"))
+			               .body("phone", equalTo("1111111111jsonFile"))
+			               .log().all();
+
+				}
+}
---
+++ rassuredTranning/src/test/java/day2/PojoPostRequest.java
@@ -0,0 +1,38 @@
+package day2;
+
+public class PojoPostRequest {
+	
+	String name;
+	String location;
+	String phone;
+	String job;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	
+	
+
+}
